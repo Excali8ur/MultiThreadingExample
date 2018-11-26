a1ccda349e86f9e2a33fcd98838d78a659aa5be7
Rianne Boumans <rianne.boumans@zuyd.nl>
2018-11-26 15:41:18 +0100

    2 Threads -> CountDown and CountUp

--- MultiThreadingExample/Program.cs
+++ MultiThreadingExample/Program.cs
@@ -2,14 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MultiThreadingExample
 {
     class Program
     {
+        int x = 0;
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            Thread t1 = new Thread(p.CountUp);
+            Thread t2 = new Thread(p.CountDown);
+            t1.Start();
+            t2.Start();
+        }
+
+        void CountUp()
+        {
+            while (x < 1000)
+            {
+                x += 1;
+                Console.WriteLine(x);
+                Thread.Sleep(100);
+            }
+        }
+
+        void CountDown()
+        {
+            while (x > -1000)
+            {
+                x -= 1;
+                Console.WriteLine(x);
+                Thread.Sleep(100);
+            }
         }
     }
 }
